67e608d52c421658b7cf83a4b8be0a3ee247a929
Omarmoahamed <Omarh9655@gmail.com>
2025-03-26 13:08:34 +0200

    Added BoundGlobal and BoundProgram

---
+++ CodeAnalysis/Binding/BoundGlobalSpace.cs
@@ -0,0 +1,25 @@
+﻿using Memo_Compiler.CodeAnalysis.Symbols;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memo_Compiler.CodeAnalysis.Binding
+{
+    internal class BoundGlobalSpace
+    {
+        public BoundGlobalSpace(BoundGlobalSpace? Previous,FunctionSymbol? MainFunc,ImmutableArray<GlobalVariableSymbol> Gvar,ImmutableArray<FunctionSymbol> Func) 
+        {
+            this.Previous = Previous;
+            this.MainFunction = MainFunc;
+            this.GlobalVariables = Gvar;
+            this.Functions = Func;
+        }
+        public BoundGlobalSpace? Previous {  get; }
+        public FunctionSymbol? MainFunction { get; }
+        public ImmutableArray<GlobalVariableSymbol> GlobalVariables { get; }
+        public ImmutableArray<FunctionSymbol> Functions { get; }
+    }
+}
---
+++ CodeAnalysis/Binding/BoundProgram.cs
@@ -0,0 +1,25 @@
+﻿using Memo_Compiler.CodeAnalysis.Symbols;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memo_Compiler.CodeAnalysis.Binding
+{
+    internal class BoundProgram
+    {
+
+        public BoundProgram(BoundProgram? Previous,FunctionSymbol MainFunc, ImmutableDictionary<FunctionSymbol, BoundBlockStatement> FunctionBodies) 
+        {
+            this.Previous = Previous;
+            this.MainFunction = MainFunc;
+            this.FunctionBodies = FunctionBodies;
+        }
+        public BoundProgram? Previous {  get; }
+        public FunctionSymbol? MainFunction { get; }
+
+        public ImmutableDictionary<FunctionSymbol,BoundBlockStatement> FunctionBodies { get; }
+    }
+}
